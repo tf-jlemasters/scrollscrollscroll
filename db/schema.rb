a4b9b6267d76eb5d870e1b314fbabc5004fc27f7
tf-jlemasters <jlemasters@tracefirst.com>
2022-03-29 13:11:11 -0600

    seeds/pagination for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_29_170549) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_29_190501) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 100.times do
     Comment.create(body: Faker::Quote.most_interesting_man_in_the_world)
+    Post.create(title: Faker::Music.band, image_url: Faker::Avatar.image)
 end
