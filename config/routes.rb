c9359ad2e7d8107be9f8a4238601dda5c01e487e
tf-jlemasters <jlemasters@tracefirst.com>
2022-03-29 11:44:39 -0600

    added load more button that generates more quotes on the page without a page refresh or redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :comments do
+    collection do
+      post :index
+    end
+  end
 end
